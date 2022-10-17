837aaf51a2399a4728e3f2c6ef8af491011a29d1
axing521 <axing@bupt.edu.cn>
2022-10-17 16:14:47 +0800

    test

---
+++ demo.js
@@ -0,0 +1,13 @@
+/***
+ * @creater:ACBash
+ * @create_time:22-10-17 14:52:9
+ * @last_modify:ACBash
+ * @modify_time:22-10-17 14:54:54
+ * @line_count:5
+ **/
+
+let a = "axing-dyj-haha";
+
+console.log(typeof a.replace("-", ":"));
+
+console.log(a);
