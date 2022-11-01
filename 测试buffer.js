b88113bee78100f801788498be2d99cc8bbd9752
axing521 <axing@bupt.edu.cn>
2022-11-01 17:11:40 +0800

    update-2022-11-1

---
+++ 测试buffer.js
@@ -0,0 +1,15 @@
+/***
+ * @creater:ACBash
+ * @create_time:22-11-1 14:18:25
+ * @last_modify:ACBash
+ * @modify_time:22-11-1 14:30:4
+ * @line_count:7
+ **/
+
+const buffer = Buffer.from('xdclass')
+
+const buf = Buffer.from('小滴课堂')
+
+console.log(buffer, buf);
+
+console.log(buf.toString());
